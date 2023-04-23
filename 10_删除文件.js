c116ddf044671baac3f1d0177c28a5a7f4eb52d4
succyag <succyag@gmail.com>
2023-04-23 19:22:14 +0800

    update

---
+++ 10_删除文件.js
@@ -0,0 +1,20 @@
+// 1. 导入 fs 模块
+const fs = require('fs')
+
+// 2. 调用 unlink 方法  unlinkSync
+// fs.unlink('./观书有感.txt', err => {
+//     if (err) {
+//         console.log('删除成功~~')
+//         return
+//     }
+//     console.log('删除成功~~')
+// })
+
+// 调用 rm 方法  14.4  rmSync
+fs.rm('./论语.txt', err => {
+    if (err) {
+        console.log('删除成功~~')
+        return
+    }
+    console.log('删除成功~~')
+})
