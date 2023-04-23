f39781cff97217ba612652ba5693f085d8ce337a
succyag <succyag@gmail.com>
2023-04-23 16:59:48 +0800

    update

---
+++ 7_流式读取.js
@@ -0,0 +1,15 @@
+// 1. 引入 fs 模块
+const fs = require('fs')
+
+// 2. 创建读取流对象
+const rs = fs.createReadStream('../资料/笑看风云.mp4')
+
+// 3. 绑定 data 事件  chunk 块儿 大块儿
+rs.on('data', chunk => {
+    console.log(chunk.length) // 65536 字节 => KB
+})
+
+// 4. end 可选事件
+rs.on('end', () => {
+    console.log('读取完成')
+})
