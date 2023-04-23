cfde8c46837484308551a8282bda4ae9bff74a69
succyag <succyag@gmail.com>
2023-04-23 20:15:40 +0800

    update

---
+++ 14_相对路径的Bug.js
@@ -0,0 +1,9 @@
+// 导入 fs 模块
+const fs = require('fs')
+
+// 相对路径参照物：命令行的工作目录
+// fs.writeFileSync('./index.html', 'test')
+
+// 绝对路径  '全局变量' 保存的是：所在文件的所在目录的绝对路径
+console.log(__dirname)
+fs.writeFileSync(__dirname + './index.html', 'test')
