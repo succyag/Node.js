8d19cea36493ff8773f361ad81ddebbf63618348
succyag <succyag@gmail.com>
2023-04-26 21:02:28 +0800

    update

--- 26_HTTP响应练习-优化.js
+++ 26_HTTP响应练习-优化.js
@@ -5,9 +5,24 @@ const fs = require('fs')
 
 // 创建服务对象
 const server = http.createServer((request, response) => {
-    // 读取文件内容
-    let html = fs.readFileSync(__dirname + '/27_table.html')
-    response.end(html) // 设置响应体
+    // 获取请求url的路径
+    let {pathname} = new URL(request.url, 'http://127.0.0.1')
+    if (pathname == '/') {
+        // 读取文件内容
+        let html = fs.readFileSync(__dirname + '/27_table.html')
+        response.end(html) // 设置响应体
+    } else if (pathname == '/28_index.css') {
+        // 读取文件内容
+        let css = fs.readFileSync(__dirname + '/28_index.css')
+        response.end(css) // 设置响应体
+    } else if (pathname == '/29_index.js') {
+        // 读取文件内容
+        let js = fs.readFileSync(__dirname + '/29_index.js')
+        response.end(js) // 设置响应体
+    } else {
+        response.statusCode = 404
+        response.end('<h1>404 Not Found</h1>')
+    }
 })
 
 // 监听端口，启动服务
