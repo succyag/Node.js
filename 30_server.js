56412a6a9308a431a15217e5fbe36926d19bd4a5
succyag <succyag@gmail.com>
2023-04-30 19:58:08 +0800

    update

--- 30_server.js
+++ 30_server.js
@@ -1,12 +1,36 @@
+/**
+ * 创建一个 HTTP 服务， 端口为 9000，满足以下需求
+ * GET /index.html          响应  page/index.html 的文件内容
+ * GET /css/app.css         响应  page/css/app.css 的文件内容
+ * GET /images/logo.png     响应  page/images/logo.png 的文件内容
+ */
+
 // 导入 http 模块
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+// 声明一个变量
+let mimes = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'text/javascript',
+    png: 'image/png',
+    jpg: 'image/jpeg',
+    gif: 'image/gif',
+    mp4: 'video/mp4',
+    mp3: 'audio/mpeg',
+    json: 'application/json'
+}
 
 // 创建服务对象
 const server = http.createServer((request, response) => {
     // 获取请求url的路径
     let {pathname} = new URL(request.url, 'http://127.0.0.1')
+    // 声明一个变量
+    let root = __dirname + '/page'
     // 拼接文件路径
-    let filePath = __dirname + '/page' + pathname
+    let filePath = root + pathname
     // 读取文件 fs 异步 API
     fs.readFile(filePath, (err, data) => {
         if (err) {
@@ -14,6 +38,17 @@ const server = http.createServer((request, response) => {
             response.statusCode = 500
             response.end('文件读取失败~~')
             return
+        }
+        // 获取文件的后缀名
+        let ext = path.extname(filePath).slice(1)
+        // 获取对应的类型
+        let type = mimes[ext]
+        if (type) {
+            // 匹配到了
+            response.setHeader('content-type', type)
+        } else {
+            // 没有匹配到
+            response.setHeader('content-type', 'application/octet-stream')
         }
         // 响应文件内容
         response.end(data)
