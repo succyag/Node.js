fc9cd6e9a7c2b91d4a1aeeffac432735e8da0a4a
succyag <succyag@gmail.com>
2023-05-04 15:31:46 +0800

    update

---
+++ 33_index.js
@@ -0,0 +1,6 @@
+// 导入模块
+const me = require('./33_me.js')
+
+// 输出me
+me.tiemo()
+me.niejiao()
---
+++ 33_me.js
@@ -0,0 +1,15 @@
+// 声明一个函数
+function tiemo () {
+    console.log('贴膜...')
+}
+
+// 捏脚
+function niejiao () {
+    console.log('捏脚...')
+}
+
+// 暴露数据
+module.exports = {
+    tiemo,
+    niejiao
+}
