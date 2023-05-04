866b62b61e9be7254ed3d7a41f8b7c4714884936
succyag <succyag@gmail.com>
2023-05-04 16:03:51 +0800

    update

--- 34_index.js
+++ 34_index.js
@@ -5,8 +5,12 @@
 // const tiemo = require('./34_me')
 
 // 导入 JSON 文件
-const duanzi = require('./34_duanzi')
-console.log(duanzi) // 对象
+// const duanzi = require('./34_duanzi')
+// console.log(duanzi) // 对象
+
+// 导入其他类型的文件
+const test = require('./34_test.abc')
+console.log(test)
 
 // 调用函数
 // tiemo()
