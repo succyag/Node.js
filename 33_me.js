1af960dfaaab8b0b1046135e60d0cb5f1acf7524
succyag <succyag@gmail.com>
2023-05-04 15:42:02 +0800

    update

--- 33_me.js
+++ 33_me.js
@@ -17,3 +17,14 @@ function niejiao () {
 // exports 暴露数据
 // exports.tiemo = tiemo
 // exports.niejiao = niejiao
+
+// 1. module.exports 可以暴露‘任意’数据
+// module.exports = 'succyag'
+// module.exports = 500
+
+// 2. 不能使用 'exports = value'的形式暴露数据
+// exports = 'succyag'
+
+// exports = module.exports = {}
+// console.log(module.exports)
+// console.log(module.exports === exports)
