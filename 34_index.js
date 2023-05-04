2249aa47540efd1804d97db31ef3d2f40c9844b3
succyag <succyag@gmail.com>
2023-05-04 15:58:09 +0800

    update

---
+++ 34_index.js
@@ -0,0 +1,12 @@
+// 导入模块
+// const tiemo = require('./34_me.js')
+
+// 省略后缀 JS
+// const tiemo = require('./34_me')
+
+// 导入 JSON 文件
+const duanzi = require('./34_duanzi')
+console.log(duanzi) // 对象
+
+// 调用函数
+// tiemo()
