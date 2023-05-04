d67e7ee31f3e21a94d44e93954a06979079e07da
succyag <succyag@gmail.com>
2023-05-04 20:41:44 +0800

    update

--- 37_npm/02_require.js
+++ 37_npm/02_require.js
@@ -1,7 +1,7 @@
 // 1. 导入 uniq 包
-// const uniq = require('uniq')
+const uniq = require('uniq')
 // const uniq = require('./node_modules/uniq')
-const uniq = require('./node_modules/uniq/uniq.js')
+// const uniq = require('./node_modules/uniq/uniq.js')
 
 // 2. 使用函数
 let arr = [1, 2, 3, 4, 5, 4, 3, 2, 1]
