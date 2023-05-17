a72aea19bc75ebd7aa98d7831b91fabd0001d3aa
succyag <succyag@gmail.com>
2023-05-17 21:26:42 +0800

    update

--- 38_express/16_generator/routes/index.js
+++ 38_express/16_generator/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// 导入
+const formidable = require('formidable')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,7 +16,27 @@ router.get('/portrait', (req, res) => {
 
 // 处理文件上传
 router.post('/portrait', (req, res) => {
-  res.send('成功')
+  // 创建 form 对象
+  const form = formidable({
+    multiples: true,
+    // 设置上传文件的保存目录
+    uploadDir: __dirname + '/../public/images',
+    // 保持文件后缀
+    keepExtensions: true
+  })
+  // 解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err)
+      return
+    }
+    // console.log(fields) // text radio checkbox select
+    // console.log(files) // files
+
+    // 服务器保存该图片的访问 URL
+    let url = '/images/' + files.portrait.newFilename // 将来将此数据保存在数据库中
+    res.send(url)
+  })
 })
 
 module.exports = router;
