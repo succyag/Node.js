d72853aa7cfcec6a0394b4f8c2f95e68a0b51529
succyag <succyag@gmail.com>
2023-05-17 21:07:53 +0800

    update

--- 38_express/16_generator/routes/index.js
+++ 38_express/16_generator/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 显示网页的 （表单）
+router.get('/portrait', (req, res) => {
+  res.render('portrait')
+})
+
+// 处理文件上传
+router.post('/portrait', (req, res) => {
+  res.send('成功')
+})
+
 module.exports = router;
