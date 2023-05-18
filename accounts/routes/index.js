f3522d0f289baead25981a8f0e5f7000c8197bc2
succyag <succyag@gmail.com>
2023-05-18 18:51:12 +0800

    update

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+// 导入 lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '/../data/db.json')
+// 获取 db 对象
+const db = low(adapter)
+// 导入 shortid
+const shortid = require('shortid')
 
 // 记账本的列表
 router.get('/account', function(req, res, next) {
@@ -12,8 +20,10 @@ router.get('/account/create', function(req, res, next) {
 
 // 新增记录
 router.post('/account', (req, res) => {
-  // 获取请求体数据
-  console.log(req.body);
+  // 生成 id
+  let id = shortid.generate()
+  // 写入文件
+  db.get('accounts').unshift({id: id, ...req.body}).write()
   res.send('添加数据')
 })
 
