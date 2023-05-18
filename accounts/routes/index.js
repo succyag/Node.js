9a52051bef4d0e19e283e2731e92d2f95fd42553
succyag <succyag@gmail.com>
2023-05-18 15:05:33 +0800

    update

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 // 记账本的列表
 router.get('/account', function(req, res, next) {
-  res.send('账本列表')
+  res.render('list')
 });
 
 router.get('/account/create', function(req, res, next) {
-  res.send('添加记录')
+  res.render('create')
 })
 
 module.exports = router;
