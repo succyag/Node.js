897f4ecfadf8845ccb6ede649364140c6844210b
succyag <succyag@gmail.com>
2023-05-19 10:44:37 +0800

    update

---
+++ 40_mongoose/01_连接数据库.js
@@ -0,0 +1,27 @@
+// 1. 安装 mongoose
+// 2. 导入 mongoose
+const mongoose = require('mongoose')
+
+// 3. 连接 mongodb 服务                      数据库的名称
+mongoose.connect('mongodb://127.0.0.1:27017/bilibili')
+
+// 4. 设置回调
+// 设置连接成功的回调
+mongoose.connection.on('open', () => {
+    console.log('连接成功');
+}) 
+
+// 设置连接错误的回调
+mongoose.connection.on('error', () => {
+    console.log('连接失败');
+}) 
+
+// 设置连接关闭的回调
+mongoose.connection.on('close', () => {
+    console.log('连接关闭');
+}) 
+
+// 关闭 mongodb 的连接
+setTimeout(() => {
+    mongoose.disconnect()
+}, 2000)
