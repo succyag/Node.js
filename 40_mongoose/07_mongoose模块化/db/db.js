7836b8383a40ac21c84dec22a927a1a1158f18cc
succyag <succyag@gmail.com>
2023-06-21 16:01:01 +0800

    update

--- 40_mongoose/07_mongoose模块化/db/db.js
+++ 40_mongoose/07_mongoose模块化/db/db.js
@@ -3,7 +3,7 @@
  * @param {*} success 数据库连接成功的回调
  * @param {*} error 数据库连接失败的回调
  */
-module.exports = function(success, error) {
+module.exports = function (success, error) {
     // 1. 安装 mongoose
     // 2. 导入 mongoose
     const mongoose = require('mongoose')
@@ -17,16 +17,16 @@ module.exports = function(success, error) {
     // 4. 设置回调
     // 设置连接成功的回调 once 一次 事件回调函数只执行一次
     mongoose.connection.once('open', () => {
-
+        success()
     })
 
     // 设置连接错误的回调
     mongoose.connection.on('error', () => {
-        success()
+        error()
     })
 
     // 设置连接关闭的回调
     mongoose.connection.on('close', () => {
-        error()
+        console.log('连接关闭');
     })
 }
